3dbc269a6adac0adacd0f3b9f0606c558f3422df
Szikszai Gusztáv <guszti5@hotmail.com>
2014-06-02 13:21:26 +0200

    Fix migration.

--- db/migrate/002_create_hr_employee_profile.rb
+++ db/migrate/002_create_hr_employee_profile.rb
@@ -2,7 +2,7 @@ class CreateHrEmployeeProfile < ActiveRecord::Migration
   def up
     User.all.each do |user|
       next if user.hr_employee_profile
-      user.hr_create_employee_profile
+      user.create_hr_employee_profile
       puts "Created employee profile for user: " + user.to_s
     end
   end
