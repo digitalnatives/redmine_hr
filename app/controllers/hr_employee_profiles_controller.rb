2a36cc9e241a08bdccc561decb9e42163d9f33dd
Szikszai Gusztáv <guszti5@hotmail.com>
2014-06-02 14:10:13 +0200

    My Holiday Requests page.
    - Sort users by lastname for edit page
    - Separate components

--- app/controllers/hr_employee_profiles_controller.rb
+++ app/controllers/hr_employee_profiles_controller.rb
@@ -9,6 +9,6 @@ class HrEmployeeProfilesController < HrAPIController
     resources = klass.all.select do |profile|
       profile.user.allowed_to_hr?
     end
-    render :json => resources
+    render :json => resources.sort_by { |prof| prof.user.lastname }
   end
 end
